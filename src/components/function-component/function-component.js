14816b51b4da22b47e6c8309b81cf16b8806a797
KingSkyLi <15566300566@163.com>
2021-01-20 13:50:28 +0800

    update

--- src/components/function-component/function-component.js
+++ src/components/function-component/function-component.js
@@ -2,19 +2,24 @@ import React,{useState,useEffect} from 'react'
 
 export default function FunctionComponent() {
     const [date,setDate] = useState(new Date())
+    const [count,setCount] = useState(0)
+    // 所有的副作用都要写到这里
     useEffect(() => {
         // 相当于componetDidMount componentWillUnmount componentDidUpdate
         const timer = setInterval(()=>{
             setDate(new Date())
         },1000)
-        return ()=>{
+        return ()=>{// 清楚副作用（componentWillUnmount）
             clearInterval(timer)
         }
-    },[])
-
+    },[])//空数组相当于didmount
+    useEffect(() => {
+       document.title = count 
+    }, [count])// 只有依赖项变化时候才会执行内部函数
     return (
         <div>
             <h3>{date.toLocaleTimeString()}</h3>
+            <h3 onClick={()=>{setCount(count + 1)}}>{count}</h3>
         </div>
     )
 }
--- src/pages/home-page/home-page.js
+++ src/pages/home-page/home-page.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import Layout from '../layout/layout'
 import store from "../../store/index"
+import FunctionComponent from "../../components/function-component/function-component"
 export default class homePage extends Component {
     componentDidMount(){
         store.subscribe(()=>{
@@ -14,6 +15,7 @@ export default class homePage extends Component {
                     {
                         content:(
                             <div>
+                                <FunctionComponent />
                                 Home-Page
                                 <p>{store.getState()}</p>
                                 <button onClick={()=>store.dispatch({type:'ADD'})}>ADD</button>
